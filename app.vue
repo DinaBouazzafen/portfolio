e95be940b53a872933e47b39925defcf005f5167
DinaBouazzafen <91288579+DinaBouazzafen@users.noreply.github.com>
2024-10-10 14:36:19 +0200

    Portfolio deel 1

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="w-full h-full no-print">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: 'Buckhain',
+  meta: [
+    { name: 'description', content: 'Portfolio' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+</script>
